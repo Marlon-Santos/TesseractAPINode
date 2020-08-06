2f8f2720b5844563faef2b4ca9032848154aa2cc
Marlon-Santos <marlonergondossantos@gmail.com>
2020-08-05 22:31:41 -0300

    tesseract node api v2

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,37 @@
 var express = require("express");
 var tesseract = require("tesseract.js");
+var multer = require("multer");
 var router = express.Router();
-
+var fs = require("fs");
+var upload = multer({ dest: "uploads/" });
 /* GET home page. */
-router.get("/tesseract", function (req, res, next) {
-  tesseract
-    .recognize(
-      "https://nova-escola-producao.s3.amazonaws.com/PVHDASREdd8TmVxrCbE2vNQm2F9JZ54mb28cYUtpFDnR4FG9cWxeDFjyPH7m/introducao",
-      "por",
-      {
-        logger: (m) => console.log(m),
+router.post("/tesseract", upload.single("file"), function (req, res, next) {
+  try {
+    if (
+      req.file.mimetype !== "image/bmp" &&
+      req.file.mimetype !== "image/jpeg" &&
+      req.file.mimetype !== "image/png" &&
+      req.file.mimetype !== "image/pbm" &&
+      req.file.mimetype !== "image/x-portable-bitmap"
+    ) {
+      console.log(req.file);
+      fs.unlinkSync(req.file.path);
+      res.json({ error: "Arquivo enviado não é suportado pelo OCR" });
+      return;
+    }
+    tesseract.recognize(req.file.path, "por").then(
+      ({ data: { text } }) => {
+        fs.unlinkSync(req.file.path);
+        var textFormat = text.split("\n").join("");
+        res.json({ text: textFormat });
+      },
+      (e) => {
+        res.json({ error: "Arquivo enviado não é suportado pelo OCR" });
       }
-    )
-    .then(({ data: { text } }) => {
-      res.json({ text: text });
-    });
+    );
+  } catch (error) {
+    res.json({ error: "Arquivo enviado não é suportado pelo OCR" });
+  }
 });
 
 module.exports = router;
